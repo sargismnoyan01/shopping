1ea5d7c0a40033e80f38d8dc3654215b968c6941
sargismnoyan01 <128826963+sargismnoyan01@users.noreply.github.com>
2024-01-22 00:11:16 +0400

    Update settings.py

--- shopping/settings.py
+++ shopping/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-9b0c&930s17jh+@5fdg9-$%=&zwv0qz%-0pkn!#qtt_2fhkn_x
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*',' 16.171.208.14']
+ALLOWED_HOSTS = ['*','16.171.146.195']
 
 
 
