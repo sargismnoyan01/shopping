7b205a2f795dc786571b4b1d291d7b1bb6b837c2
sargismnoyan01 <128826963+sargismnoyan01@users.noreply.github.com>
2024-01-24 19:53:16 +0400

    Update settings.py

--- shopping/settings.py
+++ shopping/settings.py
@@ -117,7 +117,7 @@ MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'main', 'static')),
 
-
+MEDIA_URL='/media/'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
